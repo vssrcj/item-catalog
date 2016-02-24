51a6041828da21258833c0bb14cf39892d6ba7bd
CJ <vssrcj@gmail.com>
2016-02-24 16:30:16 +0200

    Removed obsolete entries in pg_config

--- pg_config.sh
+++ pg_config.sh
@@ -1,5 +1,4 @@
 apt-get -qqy update
-apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 pip install bleach
@@ -10,10 +9,6 @@ pip install redis
 pip install passlib
 pip install itsdangerous
 pip install flask-httpauth
-su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
